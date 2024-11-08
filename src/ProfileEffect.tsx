ef8d315790faf238942fe8c4ec1754404389614b
Rio <onlytru6340@gmail.com>
2024-11-08 10:00:39 -0800

    Create ProfileEffect component and extend Taildind CSS configuration for profile-effect animation

---
+++ src/ProfileEffect.tsx
@@ -0,0 +1,12 @@
+import profile from "./assets/profile.jpeg";
+
+const ProfileEffect = () => {
+  return (
+    <div
+      className="bg-cover bg-center bg-no-repeat shadow-[inset_0_0_0_9px_rgba(225,225,225,0.3)] w-[320px] h-[450px] animate-profile-effect"
+      style={{ backgroundImage: `url(${profile})` }}
+    ></div>
+  );
+};
+
+export default ProfileEffect;
