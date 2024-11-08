f30decd4d6d0540a610c29ec5607e8f62bfd6d14
Rio <onlytru6340@gmail.com>
2024-11-08 09:48:05 -0800

    Revert "Create SocialButtons component and extend Tailwind CSS animations"
    This reverts commit 768ad7ecd6736d8f8b4b82971d3e52870ab7fe3d.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      animation: {
-        "profile-effect": "profile__effect 8s ease-in-out infinite 1s",
-      },
-      keyframes: {
-        profile__effect: {
-          "0%": {
-            borderRadius: "60% 40% 30% 70%/60% 30% 70% 40%",
-          },
-          "50%": {
-            borderRadius: "30% 60% 70% 40%/50% 60% 30% 60%",
-          },
-          "100%": {
-            borderRadius: "60% 40% 30% 70%/60% 30% 70% 40%",
-          },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
