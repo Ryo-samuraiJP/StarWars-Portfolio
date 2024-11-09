34fd0b6b7e1e1873387114a8e11aaea574902369
Rio <onlytru6340@gmail.com>
2024-11-08 23:59:03 -0800

    Create ShootingStarsBackground component and implement it to the App

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import "./App.css";
 import Home from "./Home";
 import NavBar from "./NavBar";
 import ProjectTabs from "./ProjectTabs";
+import ShootingStarsBackground from "./ShootingStarsBackground";
 
 function App() {
   return (
     <>
+      <ShootingStarsBackground />
       <NavBar />
       <Home />
       <ProjectTabs />
---
+++ src/ShootingStarsBackground.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { ShootingStars } from "./components/ui/shooting-stars";
+import { StarsBackground } from "./components/ui/stars-background";
+
+const ShootingStarsBackground = () => {
+  return (
+    <div className="fixed inset-0 z-0">
+      <ShootingStars />
+      <StarsBackground />
+    </div>
+  );
+};
+
+export default ShootingStarsBackground;
