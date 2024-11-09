90d5748f29c94df2487c4fa7d72cdb654404602e
Rio <onlytru6340@gmail.com>
2024-11-09 03:56:00 -0800

    Enhance tab styling and layout of ProjectTabs and ProjectCards components

--- src/ProjectCards.tsx
+++ src/ProjectCards.tsx
@@ -14,7 +14,7 @@ const ProjectCards = ({ genre }: ProjectCardsProps) => {
           key={project.id}
           className="flex flex-col w-1/2 px-[1.5rem] my-[1.5rem]"
         >
-          <div className="h-[35rem] border-2 px-[1.5em] border-[rgba(75,30,133,0.5)] rounded-[1.5em] bg-gradient-to-br from-[rgba(75,30,133,1)] to-[rgba(75,30,133,0.01)] text-white font-nunito flex justify-center items-left flex-col backdrop-blur-[12px] shadow-md overflow-hidden transition-all duration-300 cursor-pointer hover:transform hover:-translate-y-2 hover:shadow-lg">
+          <div className="h-[35rem] border-2 px-[1.5em] border-[rgba(75,30,133,0.5)] rounded-[1.5em] bg-gradient-to-br from-[rgba(75,30,133,1)] to-[rgba(75,30,133,0.01)] font-nunito flex justify-center items-left flex-col backdrop-blur-[12px] shadow-md overflow-hidden transition-all duration-300 cursor-pointer hover:transform hover:-translate-y-2 hover:shadow-lg">
             <div className="rounded-[0.5rem] overflow-hidden -mt-[1rem] border-2 border-gray-400">
               <img src={project.image} alt="Project" className="object-cover" />
             </div>
@@ -26,7 +26,7 @@ const ProjectCards = ({ genre }: ProjectCardsProps) => {
               {project.links.map((link, index) => (
                 <button
                   key={index}
-                  className="flex items-center gap-[0.5rem] border-2 border-white text-white py-[0.25rem] px-[0.75rem] rounded-[1rem] transition-all duration-300 hover:bg-white hover:text-black"
+                  className="flex items-center gap-[0.5rem] border-2 py-[0.25rem] px-[0.75rem] rounded-[1rem] transition-all duration-300 hover:bg-white hover:text-black"
                 >
                   <link.icon className="text-[1.2rem]" />
                   <span>{link.text}</span>
@@ -38,7 +38,7 @@ const ProjectCards = ({ genre }: ProjectCardsProps) => {
               {project.tech_used.map((tech, index) => (
                 <div key={index} className="relative group">
                   <tech.icon className="text-[1.4rem]" />
-                  <div className="absolute top-full left-1/2 transform -translate-x-1/2 mt-[0.5rem] w-max px-[0.5rem] py-[0.25rem] text-white border-2 rounded-full opacity-0 scale-50 transition-all duration-500 group-hover:opacity-100 group-hover:scale-100">
+                  <div className="absolute top-full left-1/2 transform -translate-x-1/2 mt-[0.5rem] w-max px-[0.5rem] py-[0.25rem] border-2 rounded-full opacity-0 scale-50 transition-all duration-500 group-hover:opacity-100 group-hover:scale-100">
                     {tech.name}
                   </div>
                 </div>
--- src/ProjectTabs.tsx
+++ src/ProjectTabs.tsx
@@ -1,21 +1,41 @@
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { Tab, Tabs, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import ProjectCards from "./ProjectCards";
 
 const ProjectTabs = () => {
   return (
     <div>
-      <hr className="my-16" />
+      <hr className="my-[4rem]" />
       <h1 className="font-semibold">Projects</h1>
-      <p className="mt-5 mb-10">Most recent work</p>
+      <p className="mt-[1.25rem] mb-[2.5rem]">Most recent work</p>
       <Tabs>
-        <TabList>
-          <Tab>All</Tab>
-          <Tab>Web</Tab>
-          <Tab>Mobile</Tab>
-          <Tab>AI</Tab>
-          <Tab>Others</Tab>
-        </TabList>
+        <div className="space-x-[2.5rem] mb-[3rem]">
+          <Tab>
+            <button className="border-2 py-[0.3rem] px-[1.5rem] rounded-[1rem] transition-all duration-300 hover:bg-white hover:text-black">
+              All
+            </button>
+          </Tab>
+          <Tab>
+            <button className="border-2 py-[0.3rem] px-[1.5rem] rounded-[1rem] transition-all duration-300 hover:bg-white hover:text-black">
+              Web
+            </button>
+          </Tab>
+          <Tab>
+            <button className="border-2 py-[0.3rem] px-[1.5rem] rounded-[1rem] transition-all duration-300 hover:bg-white hover:text-black">
+              Mobile
+            </button>
+          </Tab>
+          <Tab>
+            <button className="border-2 py-[0.3rem] px-[1.5rem] rounded-[1rem] transition-all duration-300 hover:bg-white hover:text-black">
+              AI
+            </button>
+          </Tab>
+          <Tab>
+            <button className="border-2 py-[0.3rem] px-[1.5rem] rounded-[1rem] transition-all duration-300 hover:bg-white hover:text-black">
+              Others
+            </button>
+          </Tab>
+        </div>
 
         <TabPanel>
           <ProjectCards genre="All" />
