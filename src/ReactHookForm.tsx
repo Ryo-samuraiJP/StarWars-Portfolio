e8fa369393cee942815d5e5af97d6a23c43d8caa
Rio <onlytru6340@gmail.com>
2024-11-24 21:18:32 -0800

    Implement email validation and status feedback in contact form

--- src/ReactHookForm.tsx
+++ src/ReactHookForm.tsx
@@ -1,32 +1,79 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 import { contactFormSchema, TContactFormSchema } from "./lib/types";
 import sendEmailWithEmailjs from "./lib/emailjs";
 import Spotlight, { SpotlightCard } from "./components/ui/spotlight-card";
-import { CiLinkedin, CiMail } from "react-icons/ci";
 import { Meteors } from "./components/ui/meteros";
 import { ImRocket } from "react-icons/im";
+import validateEmail from "./lib/emailValidator";
+import { MdError, MdCheckCircle } from "react-icons/md";
 
 const ReactHookForm = () => {
-  // Setup and configure contact form validation and submission
+  // State for email validation status
+  const [emailStatus, setEmailStatus] = useState<{
+    message: string;
+    color: string;
+  } | null>(null);
+
+  // State for form submission status
+  const [formStatus, setFormStatus] = useState<{
+    message: string;
+    color: string;
+  } | null>(null);
+
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
     reset,
-  } = useForm<TContactFormSchema>({
-    // TContactFormSchema is inferred from contactFormSchema
-    resolver: zodResolver(contactFormSchema),
-  });
+  } = useForm<TContactFormSchema>(
+    // Use zodResolver to validate form data
+    {
+      resolver: zodResolver(contactFormSchema),
+    }
+  );
 
-  // Handle form submission and reset form after submission
   const onSubmit = async (data: TContactFormSchema) => {
-    console.log(data);
+    setEmailStatus(null);
+    setFormStatus(null);
 
-    // Call sendEmailWithEmailjs function to send email
-    await sendEmailWithEmailjs(data);
+    // Validate email address using emailValidator function before sending the email with EmailJS
+    const isEmailValid = await validateEmail(data.email);
+    if (!isEmailValid) {
+      setEmailStatus({
+        message: "Email address is invalid",
+        color: "text-red-600",
+      });
+      return;
+    } else {
+      setEmailStatus({
+        message: "Email address is valid",
+        color: "text-green-600",
+      });
+    }
 
-    reset();
+    // Send email with EmailJS and handle success/failure
+    await sendEmailWithEmailjs(data)
+      .then(() => {
+        setFormStatus({
+          message: "Submitted the form successfully!",
+          color: "text-green-600",
+        });
+
+        // Reset the form fields, emailStatus, and formStatus ONLY after a delay of successful form submission
+        setTimeout(() => {
+          setEmailStatus(null);
+          setFormStatus(null);
+          reset();
+        }, 5000);
+      })
+      .catch(() => {
+        setFormStatus({
+          message: "Failed to submit the form. Please try again",
+          color: "text-red-600",
+        });
+      });
   };
 
   return (
@@ -34,8 +81,8 @@ const ReactHookForm = () => {
       <SpotlightCard>
         <Meteors number={15} />
         <form
-          onSubmit={handleSubmit(onSubmit)} // onSubmit will trigger validation
-          className="flex flex-col gap-y-[1.5rem] py-[2rem] bg-slate-900"
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-y-[1.5rem] py-[2rem] pb-[2.75rem] bg-slate-900"
         >
           <div className="flex flex-row gap-x-[0.5rem] items-center justify-center text-[1.25rem]">
             <ImRocket />
@@ -44,10 +91,10 @@ const ReactHookForm = () => {
           <input
             type="text"
             placeholder="Name"
-            {...register("name")} // register will trigger validation on change
+            {...register("name")}
             className="rounded-[1rem] p-[1rem] mx-[5rem] bg-transparent border border-slate-500"
           />
-          {/* Display error message if name is invalid */}
+          {/* Display error message if name is not provided */}
           {errors.name && (
             <span className="text-left text-sm font-medium px-[6rem] -my-[1rem] text-red-600">
               {`${errors.name.message}`}
@@ -59,10 +106,27 @@ const ReactHookForm = () => {
             {...register("email")}
             className="rounded-[1rem] p-[1rem] mx-[5rem] bg-transparent border border-slate-500"
           />
-          {/* Display error message if email is invalid */}
+          {/* Display error message if email is not provided */}
           {errors.email && (
             <span className="text-left text-sm font-medium px-[6rem] -my-[1rem] text-red-600">
               {`${errors.email.message}`}
+            </span>
+          )}
+          {/* Display email validation status */}
+          {emailStatus && (
+            <span
+              className={`text-left text-sm font-medium px-[6rem] -my-[1rem] ${emailStatus.color}`}
+            >
+              <div className="flex flex-row gap-x-[0.25rem] items-center">
+                {emailStatus.color === "text-red-600" ? (
+                  // Display error icon if email is invalid
+                  <MdError className="text-[1.125rem]" />
+                ) : (
+                  // Display check icon if email is valid
+                  <MdCheckCircle className="text-[1.125rem]" />
+                )}
+                {emailStatus.message}
+              </div>
             </span>
           )}
           <input
@@ -71,7 +135,7 @@ const ReactHookForm = () => {
             {...register("subject")}
             className="rounded-[1rem] p-[1rem] mx-[5rem] bg-transparent border border-slate-500"
           />
-          {/* Display error message if subject is invalid */}
+          {/* Display error message if subject is not provided */}
           {errors.subject && (
             <span className="text-left text-sm font-medium px-[6rem] -my-[1rem] text-red-600">
               {`${errors.subject.message}`}
@@ -82,43 +146,36 @@ const ReactHookForm = () => {
             {...register("message")}
             className="rounded-[1rem] p-[1rem] pb-[6rem] mx-[5rem] resize-none bg-transparent border border-slate-500"
           />
-          {/* Display error message if message is invalid */}
+          {/* Display error message if message is not provided */}
           {errors.message && (
             <span className="text-left text-sm font-medium px-[6rem] -my-[1rem] text-red-600">
               {`${errors.message.message}`}
             </span>
           )}
           <button
-            disabled={isSubmitting} // Disable button while submitting form data to server
+            disabled={isSubmitting}
             type="submit"
-            className="p-[0.75rem] my-[0.75rem] mx-[5rem] rounded-md bg-slate-500 text-white"
+            className="p-[0.75rem] mt-[0.75rem] mx-[5rem] rounded-md bg-slate-500 text-white"
           >
             Submit
           </button>
-          <hr className="mx-[2.5rem] bg-slate-600 border-0 h-px" />
-          <p className="text-slate-500 mb-[0.5rem]">
-            or reach out to me through LinkedIn or email
-          </p>
-          <div className="flex flex-col gap-y-[1rem] items-center text-slate-500">
-            <a
-              href="https://linkedin.com/in/ryoichihomma/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex flex-row gap-x-[0.25rem] items-center hover:text-white hover:underline transition-all duration-300 ease-in-out"
+          {/* Display form submission status */}
+          {formStatus && (
+            <span
+              className={`text-left text-sm font-medium px-[6rem] -my-[1rem] ${formStatus.color}`}
             >
-              <CiLinkedin className="text-[1.75rem]" />
-              <span>linkedin.com/in/ryoichihomma/</span>
-            </a>
-            <a
-              href="mailto:r.homma.inbox@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex flex-row gap-x-1 items-center hover:text-white hover:underline transition-all duration-300 ease-in-out"
-            >
-              <CiMail className="text-[1.5rem]" />
-              <span>r.homma.inbox@gmail.com</span>
-            </a>
-          </div>
+              <div className="flex flex-row gap-x-[0.25rem] items-center">
+                {formStatus.color === "text-red-600" ? (
+                  // Display error icon if form submission failed
+                  <MdError className="text-[1.125rem]" />
+                ) : (
+                  // Display check icon if form submission succeeded
+                  <MdCheckCircle className="text-[1.125rem]" />
+                )}
+                {formStatus.message}
+              </div>
+            </span>
+          )}
         </form>
       </SpotlightCard>
     </Spotlight>
--- src/lib/emailValidator.ts
+++ src/lib/emailValidator.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const ZEROBOUNCE_API_KEY = "ec3a85a71bdd40b08d0729b3c78f59c2";
 
+// Validate email address using ZeroBounce API
 const validateEmail = async (email: string): Promise<boolean> => {
   try {
     const response = await axios.get(
