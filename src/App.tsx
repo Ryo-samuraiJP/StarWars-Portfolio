8bbe82e93cdff9d1b7a6ea67b456fe15c2497e23
Rio <onlytru6340@gmail.com>
2024-11-26 14:50:50 -0800

    Refactor App component to implement routing and organize sections for improved navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
 import "./App.css";
-import Home from "./Home";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./sections/Home";
 import NavBar from "./NavBar";
-import ProjectTabs from "./ProjectTabs";
 import ShootingStarsBackground from "./ShootingStarsBackground";
+import TechSkillSection from "./sections/TechSkillSection";
+import ProjectSection from "./sections/ProjectSection";
+import ServiceSection from "./sections/ServiceSection";
+import CredentialSection from "./sections/CredentialSection";
+import TechBlogSection from "./sections/TechBlogSection";
+import ContactSection from "./sections/ContactSection";
 
 function App() {
   return (
     <>
       <ShootingStarsBackground />
-      <NavBar />
-      <Home />
-      <ProjectTabs />
+      <BrowserRouter>
+        <NavBar />
+        <section id="home">
+          <Home />
+        </section>
+        <section id="project">
+          <ProjectSection />
+        </section>
+        <section id="skill">
+          <TechSkillSection />
+        </section>
+        <section id="service">
+          <ServiceSection />
+        </section>
+        <section id="credential">
+          <CredentialSection />
+        </section>
+        <section id="blog">
+          <TechBlogSection />
+        </section>
+        <section id="contact">
+          <ContactSection />
+        </section>
+      </BrowserRouter>
     </>
   );
 }
