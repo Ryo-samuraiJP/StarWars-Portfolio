5b89bca1174023eed559b88b1632c23a5d842267
Rio <onlytru6340@gmail.com>
2024-11-28 13:58:26 -0800

    Add SocialIcons component with animated hover effects and links to social profiles

---
+++ src/components/ui/SocialIcons.tsx
@@ -0,0 +1,46 @@
+import { FaLinkedin, FaGithub, FaYoutube, FaDev } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import { SocialIconsAnimation } from "./social-icons-animation";
+
+const iconData = [
+  {
+    name: "LinkedIn",
+    icon: <FaLinkedin className="size-full" />,
+    hoverColor: "text-[#0A66C2]",
+    href: "https://www.linkedin.com/in/ryoichihomma/",
+  },
+  {
+    name: "GitHub",
+    icon: <FaGithub className="size-full" />,
+    hoverColor: "text-[#6E5494]",
+    href: "https://github.com/Ryo-samuraiJP",
+  },
+  {
+    name: "DEV",
+    icon: <FaDev className="size-full" />,
+    hoverColor: "text-[#0A0A0A]",
+    href: "https://dev.to/ryoichihomma",
+  },
+  {
+    name: "YouTube",
+    icon: <FaYoutube className="size-full" />,
+    hoverColor: "text-[#FF0000]",
+    href: "https://www.youtube.com/@rh.project_gallery",
+  },
+  {
+    name: "Email",
+    icon: <IoMdMail className="size-full" />,
+    hoverColor: "text-[#00B2FF]",
+    href: "mailto:r.homma.inbox@gmail.com",
+  },
+];
+
+const SocialIcons = () => {
+  return (
+    <div>
+      <SocialIconsAnimation items={iconData} />
+    </div>
+  );
+};
+
+export default SocialIcons;
