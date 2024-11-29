829823cd8005b5dc1559cf797c22b82d49ded2b8
Rio <onlytru6340@gmail.com>
2024-11-28 17:52:54 -0800

    Update CredentialSection content and styling for improved clarity and thematic consistency

--- src/sections/CredentialSection.tsx
+++ src/sections/CredentialSection.tsx
@@ -4,9 +4,12 @@ const CredentialSection = () => {
   return (
     <section id="credential">
       <hr className="mt-[5.5rem] mb-[4rem]" />
-      <h1 className="font-semibold">Credentials</h1>
-      <p className="mt-[1.25rem] mb-[2.5rem]">
-        Professional and academic backgrounds and certifications
+      <p className="text-lg my-[0.5rem] font-bold text-yellow-500">
+        Episode IV
+      </p>
+      <h1 className="text-yellow-500 star-wars-font">credentials</h1>
+      <p className="mt-[1rem] mb-[2.5rem] text-yellow-500">
+        Professional & educational backgrounds and certifications
       </p>
       <CredentialTimeline />
     </section>
