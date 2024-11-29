be7d05a68bc3c67cfacca64d5c2105a4ab563cd8
Rio <onlytru6340@gmail.com>
2024-11-28 17:53:53 -0800

    Update Footer content and styling for improved clarity and thematic consistency

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,25 +4,23 @@ const Footer = () => {
   return (
     <footer>
       <hr className="mt-[5.5rem] my-[3rem]" />
-      <h1 className="text-yellow-500 star-wars-font">
-        <p>Ryoichi</p>
-        <p>Homma</p>
-      </h1>
-      <div className="text-yellow-500 pt-[0.5rem]">
-        <div className="font-bold">
-          <p>Episode IV</p>
-          <p>A NEW CODER</p>
-        </div>
-        <p className="pt-[1rem] px-[20rem]">
-          Reach out to me today and let's discuss how I can help you achieve
-          your goals. Let's build something impactful together!
-        </p>
-      </div>
+      <p className="text-lg my-[0.5rem] font-bold text-yellow-500">
+        Episode VII
+      </p>
+      <h1 className="text-yellow-500 star-wars-font">the code awakens</h1>
+      <p className="mt-[1rem] px-[20rem] text-yellow-500">
+        Reach out to me today and let's discuss how I can help you achieve your
+        goals. Let's build something impactful together!
+      </p>
       <div className="flex justify-center py-[3.5rem]">
         <SocialIcons />
       </div>
-      <p>&#169; 2024 Ryoichi (Rio) Homma.</p>
-      <p>May the Code be with you.</p>
+      <div className="flex flex-row justify-center">
+        <p>&#169; 2024 Ryoichi (Rio) Homma. &nbsp;</p>
+        <p>May the</p>
+        <p className="text-shadow-neon">&nbsp;Code&nbsp;</p>
+        <p>be with you.</p>
+      </div>
     </footer>
   );
 };
