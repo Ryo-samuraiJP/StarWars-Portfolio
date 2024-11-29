c93c4f39ccdd7ca99eeae2b3685ff08fed54f38f
Rio <onlytru6340@gmail.com>
2024-11-28 17:53:42 -0800

    Update ContactSection content and styling for improved clarity and thematic consistency

--- src/sections/ContactSection.tsx
+++ src/sections/ContactSection.tsx
@@ -4,9 +4,12 @@ const ContactSection = () => {
   return (
     <section id="contact">
       <hr className="mt-[5.5rem] mb-[4rem]" />
-      <h1 className="font-semibold">Contact</h1>
-      <p className="mt-[1.25rem] mb-[2.5rem]">
-        Feel free to reach out to me for any inquiries
+      <p className="text-lg my-[0.5rem] font-bold text-yellow-500">
+        Episode VI
+      </p>
+      <h1 className="text-yellow-500 star-wars-font">contact me</h1>
+      <p className="mt-[1rem] mb-[2.5rem] text-yellow-500">
+        Feel free to get in touch for any inquiries
       </p>
       <ReactHookForm />
     </section>
