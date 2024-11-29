0f00a832cfe1e4c1f8351d02c9e9e8cd4bd17bb1
Rio <onlytru6340@gmail.com>
2024-11-28 17:52:09 -0800

    Update ServiceSection content and styling for improved clarity and thematic consistency for consistency

--- src/sections/ServiceSection.tsx
+++ src/sections/ServiceSection.tsx
@@ -4,9 +4,12 @@ const ServiceSection = () => {
   return (
     <section id="service">
       <hr className="my-[4rem]" />
-      <h1 className="font-semibold">Services</h1>
-      <p className="mt-[1.25rem] mb-[2.5rem]">
-        What I offer and how I contribute to your organization
+      <p className="text-lg my-[0.5rem] font-bold text-yellow-500">
+        Episode III
+      </p>
+      <h1 className="text-yellow-500 star-wars-font">services</h1>
+      <p className="mt-[1rem] mb-[2.5rem] text-yellow-500">
+        What I offer and how I help you achieve your goals
       </p>
       <ServiceCards />
     </section>
