2076835dc7167394f4bc5b42ad36127f0fe10a4e
Rio <onlytru6340@gmail.com>
2024-11-28 17:51:31 -0800

    Update TechSkillSection styling and content for enhanced clarity and thematic consistency for consistency

--- src/sections/TechSkillSection.tsx
+++ src/sections/TechSkillSection.tsx
@@ -5,15 +5,18 @@ const TechSkillSection = () => {
   return (
     <section id="technical-skill">
       <hr className="mt-[5.5rem] mb-[4rem]" />
-      <h1 className="font-semibold">Technical Skills</h1>
-      <p className="mt-[1.25rem] mb-[2.5rem]">
-        Technical skills and primary technologies
+      <p className="text-lg my-[0.5rem] font-bold text-yellow-500">
+        Episode II
+      </p>
+      <h1 className="text-yellow-500 star-wars-font">technical skills</h1>
+      <p className="mt-[1rem] mb-[2.5rem] text-yellow-500">
+        Skills cloud and primary technologies
       </p>
       <div className="w-full max-w-[72rem] mx-auto px-[1rem] md:px-[1.5rem]">
         <div className="flex flex-col lg:flex-row items-start lg:items-center">
           <div className="relative inline-flex">
             <div
-              className="w-[50%] h-[50%] border absolute inset-0 m-auto blur-3xl -z-10 rounded-full bg-indigo-700"
+              className="w-[50%] h-[50%] border absolute inset-0 m-auto blur-3xl -z-10 rounded-full bg-violet-800"
               aria-hidden="true"
             ></div>
             <div className="grow">
