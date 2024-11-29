fc20c016745505cbf7568bbb53e5ea7bd52a9ff1
Rio <onlytru6340@gmail.com>
2024-11-28 17:49:29 -0800

    Revamp ProjectSection styling and content for improved clarity and aesthetics for consistency

--- src/sections/ProjectSection.tsx
+++ src/sections/ProjectSection.tsx
@@ -3,9 +3,12 @@ import ProjectTabs from "../ProjectTabs";
 const ProjectSection = () => {
   return (
     <section id="project">
-      <hr className="my-[4rem]" />
-      <h1 className="font-semibold">Featured Projects</h1>
-      <p className="mt-[1.25rem] mb-[2.5rem]">Most recent work</p>
+      <hr className="my-[4rem] mb-[2rem]" />
+      <p className="text-lg my-[0.5rem] font-bold text-yellow-500">Episode I</p>
+      <h1 className="text-yellow-500 star-wars-font">featured projects</h1>
+      <p className="mt-[1rem] mb-[2.5rem] text-yellow-500">
+        Latest works categorized by genre
+      </p>
       <ProjectTabs />
     </section>
   );
