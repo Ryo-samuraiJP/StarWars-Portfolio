257f823186ae958b360b94b6922a227ba5baf614
Rio <onlytru6340@gmail.com>
2024-11-28 17:53:12 -0800

    Update TechBlogSection content and styling for improved clarity and thematic consistency

--- src/sections/TechBlogSection.tsx
+++ src/sections/TechBlogSection.tsx
@@ -4,8 +4,11 @@ const TechBlogSection = () => {
   return (
     <section id="tech-blog">
       <hr className="mt-[5.5rem] mb-[4rem]" />
-      <h1 className="font-semibold">Tech Blog</h1>
-      <p className="mt-[1.25rem] mb-[2.5rem]">Latest posts on DEV Community</p>
+      <p className="text-lg my-[0.5rem] font-bold text-yellow-500">Episode V</p>
+      <h1 className="text-yellow-500 star-wars-font">tech blog</h1>
+      <p className="mt-[1rem] mb-[2.5rem] text-yellow-500">
+        Latest posts on the DEV Community
+      </p>
       <SwiperBlog />
     </section>
   );
