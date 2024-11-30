00dec1b03e0a3350452b246aaec3e90cce8330f4
Rio <onlytru6340@gmail.com>
2024-11-29 23:30:12 -0800

    Add NumbersCounterAnimation component for animated number display

---
+++ src/components/ui/numbers-counter-animation.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef } from "react";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+import { cn } from "../../lib/utils";
+
+export function NumbersCounterAnimation({
+  value,
+  direction = "up",
+  delay = 0,
+  className,
+  decimalPlaces = 0,
+}: {
+  value: number;
+  direction?: "up" | "down";
+  className?: string;
+  delay?: number; // delay in seconds
+  decimalPlaces?: number;
+}) {
+  const ref = useRef<HTMLSpanElement>(null);
+  const motionValue = useMotionValue(direction === "down" ? value : 0); // Set initial value to 0 if direction is down
+  // Spring animation for the counter value
+  const springValue = useSpring(motionValue, {
+    damping: 60,
+    stiffness: 100,
+  });
+  const isInView = useInView(ref, { once: true, margin: "0px" });
+
+  // Update the counter value when the element is in view
+  useEffect(() => {
+    if (isInView) {
+      setTimeout(() => {
+        motionValue.set(direction === "down" ? 0 : value);
+      }, delay * 1000);
+    }
+  }, [motionValue, isInView, delay, value, direction]);
+
+  // Update the counter value when the value prop changes
+  useEffect(
+    () =>
+      springValue.on("change", (latest) => {
+        if (ref.current) {
+          // Format the number with commas and decimal places
+          ref.current.textContent = Intl.NumberFormat("en-US", {
+            minimumFractionDigits: decimalPlaces,
+            maximumFractionDigits: decimalPlaces,
+          }).format(Number(latest.toFixed(decimalPlaces)));
+        }
+      }),
+    [springValue, decimalPlaces]
+  );
+
+  return <span className={cn("tabular-nums", className)} ref={ref} />;
+}
