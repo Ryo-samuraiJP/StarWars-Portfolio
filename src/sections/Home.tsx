17164463555b849f6fe113747e25cdac09bddbcb
Rio <onlytru6340@gmail.com>
2024-12-02 23:36:17 -0800

    Update Home component layout and improve responsiveness

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -9,31 +9,35 @@ const Home = () => {
   return (
     <section id="home" className="flex flex-col items-center justify-center">
       <div className="flex flex-col md:flex-row items-center justify-center w-[90%]">
+        <div className="flex mt-[4rem] justify-center md:order-2 lg:mr-10 2xl:mr-20">
+          <ProfileEffect />
+        </div>
         <div className="flex-1 p-[1rem] mt-[4rem]">
-          <div className="space-y-4">
-            <div className="flex font-medium text-4xl text-left text-gray-200">
-              <p>Hi, I'm Ryoichi&nbsp;</p>
+          <div className="space-y-3 md:space-y-4 md:-mr-36">
+            <div className="flex font-medium -mt-14 md:mt-0 text-lg md:text-4xl justify-center md:justify-start text-gray-200">
+              <p>Hi, I'm&nbsp;</p>
               <p className="text-shadow-neon font-bold text-white">Rio</p>
-              <p>&nbsp;Homma</p>
+              <p>&nbsp;based in Canada</p>
             </div>
-            <div className="flex flex-row text-6xl text-left font-bold">
-              <div className="text-white text-shadow-neon star-wars-font">
-                <Typewriter
-                  options={{
-                    strings: [
-                      "software engineer",
-                      "web developer",
-                      "ai evaluator",
-                    ],
-                    delay: 50,
-                    deleteSpeed: 30,
-                    loop: true,
-                    autoStart: true,
-                  }}
-                />
-              </div>
+            <div
+              className="flex flex-row -mx-16 md:mx-0 text-3xl md:text-[2.5rem] lg:text-6xl justify-center md:justify-start
+              text-white text-shadow-neon star-wars-font"
+            >
+              <Typewriter
+                options={{
+                  strings: [
+                    "software engineer",
+                    "web developer",
+                    "ai evaluator",
+                  ],
+                  delay: 50,
+                  deleteSpeed: 30,
+                  loop: true,
+                  autoStart: true,
+                }}
+              />
             </div>
-            <p className="w-[85%] py-[1.25rem] text-left">
+            <p className="-mx-9 md:mx-0 md:w-[70%] lg:w-[75%] 2xl:w-[70%] md:py-[1.25rem] text-center md:text-left">
               While I specialize in front-end development with React.js,
               TypeScript/JavaScript, and Tailwind CSS, I am actively expanding
               my expertise to full-stack development by enhancing my skills in
@@ -45,14 +49,13 @@ const Home = () => {
               prompting. Additionally, I am a huge MLB fan who is interested in
               sports science and baseball data analytics!
             </p>
-            <div className="flex flex-row items-center gap-[3rem]">
+            <div className="flex flex-col pt-4 md:pt-0 md:flex-row items-center gap-[2.5rem] md:gap-[4.5rem] lg:gap-[3rem]">
               <DownloadButton />
-              <SocialIcons />
+              <div className="-mx-12 lg:mx-0 justify-start">
+                <SocialIcons />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex mt-[4rem] justify-center">
-          <ProfileEffect />
         </div>
       </div>
       <div className="flex flex-wrap items-center pt-[2rem] pb-[3rem]">
