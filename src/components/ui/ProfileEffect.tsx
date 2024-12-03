927c1db2dcfa8e37da5f36fa6bb6c3f66131cfd4
Rio <onlytru6340@gmail.com>
2024-12-02 23:33:05 -0800

    Update ProfileEffect component to make it responsive

--- src/components/ui/ProfileEffect.tsx
+++ src/components/ui/ProfileEffect.tsx
@@ -3,7 +3,8 @@ import profile from "../../assets/images/profile.jpeg";
 const ProfileEffect = () => {
   return (
     <div
-      className="bg-cover bg-center bg-no-repeat shadow-[inset_0_0_0_9px_rgba(225,225,225,0.3)] w-[320px] h-[450px] animate-profile-effect"
+      className="bg-cover bg-center bg-no-repeat shadow-[inset_0_0_0_9px_rgba(225,225,225,0.3)] 
+        w-[250px] h-[350px] lg:w-[300px] lg:h-[440px]  animate-profile-effect"
       style={{ backgroundImage: `url(${profile})` }}
     ></div>
   );
