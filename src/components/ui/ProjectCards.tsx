68a7d4f50f41a9d532809e91be19cfb5220b538e
Rio <onlytru6340@gmail.com>
2024-12-03 16:55:49 -0800

    Update ProjectCards component to make it responsive

--- src/components/ui/ProjectCards.tsx
+++ src/components/ui/ProjectCards.tsx
@@ -8,44 +8,46 @@ const ProjectCards = ({ genre }: ProjectCardsProps) => {
   const filteredProjects = filterProjects(genre);
 
   return (
-    <div className="flex flex-wrap mx-[2rem]">
+    <div className="md:flex md:flex-wrap md:mx-8">
       {filteredProjects.map((project) => (
-        <div
-          key={project.id}
-          className="flex flex-col w-1/2 px-[1.5rem] my-[1.5rem]"
-        >
+        <div key={project.id} className="flex flex-col lg:w-[50%] md:px-6 my-6">
           <div
-            className="h-[35rem] px-[1.5em] border-2 border-[rgba(75,30,133,0.5)] rounded-[1.5em] bg-gradient-to-br from-[rgba(75,30,133,1)] to-[rgba(75,30,133,0.01)] 
-            font-nunito flex justify-center items-left flex-col backdrop-blur-[12px] shadow-md overflow-hidden transition-all duration-300 cursor-pointer hover:transform hover:-translate-y-2 hover:shadow-lg"
+            className="h-[29.5rem] md:h-[37rem] px-6 border-2 border-[rgba(75,30,133,0.5)] rounded-3xl bg-gradient-to-br from-[rgba(75,30,133,1)] to-[rgba(75,30,133,0.01)] 
+            font-nunito flex justify-center items-left flex-col backdrop-blur-[.75rem] shadow-md overflow-hidden transition-all duration-300 cursor-pointer hover:transform hover:-translate-y-2"
           >
-            <div className="rounded-[0.5rem] overflow-hidden -mt-[1rem] border-2 border-gray-400">
+            <div className="rounded-lg overflow-hidden -mt-6 md:-mt-4 border-2 border-gray-400">
               <img src={project.image} alt="Project" className="object-cover" />
             </div>
-            <div className="text-[1.5rem] font-semibold text-left mt-[0.5rem]">
+            <div className="text-xl md:text-2xl font-semibold text-left mt-2 md:mt-4">
               {project.title}
             </div>
-            <p className="text-[1rem] font-light text-left">{project.desc}</p>
-            <div className="flex gap-x-[1.5rem] items-center my-[1rem]">
+            <p className="text-base font-light text-left">{project.desc}</p>
+            <div className="grid grid-cols-2 md:flex md:flex-row gap-2 md:gap-6 pr-3 md:pr-0 my-4">
               {project.links.map((link, index) => (
                 <button
                   key={index}
-                  className="flex items-center gap-[0.5rem] border-2 py-[0.25rem] px-[0.75rem] rounded-[1rem] transition-all duration-300 hover:bg-white hover:text-black"
+                  className="flex items-center justify-center gap-2 border-2 py-1 px-3 rounded-2xl transition-all duration-300 hover:bg-white hover:text-black"
                 >
                   <link.icon className="text-[1.2rem]" />
                   <span>{link.text}</span>
                 </button>
               ))}
             </div>
-            <div className="flex items-center gap-x-[1.25rem] mt-[0.75rem]">
-              <p className="text-bold">Technologies Used:</p>
-              {project.tech_used.map((tech, index) => (
-                <div key={index} className="relative group">
-                  <tech.icon className="text-[1.4rem]" />
-                  <div className="absolute top-full left-1/2 transform -translate-x-1/2 mt-[0.5rem] w-max px-[0.5rem] py-[0.25rem] border-2 rounded-full opacity-0 scale-50 transition-all duration-500 group-hover:opacity-100 group-hover:scale-100">
-                    {tech.name}
+            <div className="flex items-center gap-x-3 md:gap-x-4 mt-3">
+              <p className="text-bold text-left ">Skill Used:</p>
+              <div className="flex flex-row gap-2 md:gap-3">
+                {project.tech_used.map((tech, index) => (
+                  <div key={index} className="relative group">
+                    <tech.icon className="text-[1.4rem]" />
+                    <div
+                      className="absolute top-full left-[50%] transform -translate-x-[50%] mt-2 w-max px-2 py-1
+                      border-2 rounded-full opacity-0 scale-50 transition-all duration-500 group-hover:opacity-100 group-hover:scale-100"
+                    >
+                      {tech.name}
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
