07dadc0d8542253fbbe3ae674a678ef88daa80d7
Rio <onlytru6340@gmail.com>
2024-12-03 17:40:19 -0800

    Update SkillProgressBar component for improved layout and responsiveness

--- src/components/ui/SkillProgressBar.tsx
+++ src/components/ui/SkillProgressBar.tsx
@@ -4,41 +4,41 @@ import Spotlight, { SpotlightCard } from "./spotlight-card";
 
 const SkillProgressBar = () => {
   return (
-    <div className="w-full max-w-[72rem] mx-auto px-[1rem] md:px-[1.5rem]">
-      <Spotlight className="max-w-sm mx-auto grid gap-[1.5rem] lg:grid-cols-1 items-start lg:max-w-none group">
+    <div className="flex md:px-6">
+      <Spotlight className="grid gap-6 lg:grid-cols-1 items-start group">
         {skillsData.map((category) => (
           <SpotlightCard key={category.id} className="w-full lg:w-[170%]">
             <div
               className="relative h-full border border-[rgba(75,30,133,0.5)] rounded-[inherit] 
-              bg-gradient-to-br from-[rgba(75,30,133,0.5)] to-[rgb(0,0,0)] p-[0.5rem] z-20 overflow-hidden"
+              bg-gradient-to-br from-[rgba(75,30,133,0.5)] to-[rgb(0,0,0)] p-2 z-20 overflow-hidden"
             >
               {/* white blur */}
               <div
-                className="absolute mb-[19.5rem] -top-[11rem] translate-y-1/2 left-1/2 -translate-x-1/2 pointer-events-none -z-10 w-1/2 aspect-square lg:top-[0.75rem]"
+                className="absolute mb-[19.5rem] -top-44 translate-y-[50%] left-[50%] -translate-x-[50%] pointer-events-none -z-10 w-[50%] aspect-square lg:top-3"
                 aria-hidden="true"
               >
-                <div className="absolute inset-0 translate-z-0 bg-slate-700 rounded-full blur-[40px]"></div>
+                <div className="absolute inset-0 translate-z-0 bg-slate-700 rounded-full blur-[2.5rem]"></div>
               </div>
               <div className="flex flex-col h-full items-center text-center">
-                <div className="relative inline-flex p-[1rem]">
+                <div className="relative inline-flex p-4">
                   {/* blue blur */}
                   <div
                     className="w-[40%] h-[40%] border absolute inset-0 m-auto -translate-y-[10%] blur-3xl -z-10 rounded-full bg-purple-600"
                     aria-hidden="true"
                   ></div>
                   <div className="grow">
-                    <h3 className="text-[1.125rem] font-semibold text-gray-200 mb-[1rem]">
+                    <h3 className="text-[1.125rem] font-semibold text-gray-200 mb-4">
                       <p>{category.category}</p>
                     </h3>
                     {/* skill icon grid */}
-                    <div className="grid grid-cols-3 gap-4 lg:grid-cols-6 justify-center">
+                    <div className="grid grid-cols-3 gap-4 md:grid-cols-6 justify-center">
                       {category.skills.map((skill, index) => (
                         <div
                           key={index}
                           className="flex flex-col items-center m-2 lg:m-0"
                         >
                           <div className="relative flex items-center justify-center">
-                            <div className="relative flex items-center justify-center w-[4rem] h-[4rem]">
+                            <div className="relative flex items-center justify-center w-16 h-16">
                               <svg
                                 className="w-[100%] h-[100%] transform rotate-[-90deg] "
                                 viewBox="0 0 200 200"
@@ -82,7 +82,7 @@ const SkillProgressBar = () => {
                               </div>
                             </div>
                           </div>
-                          <p className="text-sm mt-[0.25rem] text-gray-200">
+                          <p className="text-sm mt-1 text-gray-200">
                             {skill.name}
                           </p>
                           <p className="text-sm font-light text-gray-400">
