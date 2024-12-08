356d39d6536195048f408c4eaa2c43bee0b54d5e
Rio <onlytru6340@gmail.com>
2024-12-07 23:55:15 -0800

    Enhance ProfileEffect component with responsive animations using framer-motion

--- src/components/ui/ProfileEffect.tsx
+++ src/components/ui/ProfileEffect.tsx
@@ -1,12 +1,28 @@
+import { useMediaQuery } from "react-responsive";
 import profile from "../../assets/images/profile.jpeg";
+import { motion } from "framer-motion";
 
 const ProfileEffect = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
+
   return (
-    <div
+    <motion.div
       className="bg-cover bg-center bg-no-repeat shadow-[inset_0_0_0_9px_rgba(225,225,225,0.3)] 
         w-[12.5rem] h-[18.75rem] md:w-[15.625rem] md:h-[21.875rem] lg:w-[18.75rem] lg:h-[27.5rem] animate-profile-effect"
       style={{ backgroundImage: `url(${profile})` }}
-    ></div>
+      initial={{
+        opacity: 0,
+        y: isMobile ? -150 : 0,
+        x: isMobile ? 0 : 200,
+      }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+        x: 0,
+      }}
+      transition={{ delay: isMobile ? 0 : 0.25, duration: 1.5 }}
+      viewport={{ once: true }}
+    ></motion.div>
   );
 };
 
