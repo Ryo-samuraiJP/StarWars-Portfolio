a69ea78373a2735af9147db1341488a205418914
Rio <onlytru6340@gmail.com>
2024-12-07 23:56:41 -0800

    Enhance ScrollDown component with framer-motion for responsive animations

--- src/components/ui/ScrollDown.tsx
+++ src/components/ui/ScrollDown.tsx
@@ -1,12 +1,29 @@
+import { motion } from "framer-motion";
+import { useMediaQuery } from "react-responsive";
+
 const ScrollDown = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
+
   return (
-    <div className="relative w-[30px] h-[50px] ml-[15px] border-[3px] rounded-[50px] mb-4 cursor-pointer">
-      <div className="absolute bottom-[30px] left-1/2 w-[6px] h-[6px] ml-[-3px] bg-white rounded-full animate-scrolldown shadow-[0px_-5px_3px_1px_rgba(42,84,112,0.4)]"></div>
-      <div className="flex flex-col items-center mt-[48px] pt-[6px]">
-        <div className="w-[10px] h-[10px] border-[3px] border-gray-100 border-t-0 border-l-0 rotate-45 animate-arrow"></div>
-        <div className="w-[10px] h-[10px] border-[3px] border-gray-100 border-t-0 border-l-0 rotate-45 animate-arrow-delay"></div>
+    <motion.div
+      className="relative w-[1.875rem] h-[3.125rem] ml-[0.938rem] border-[0.188rem] rounded-[3.125rem] mb-4 cursor-pointer"
+      initial={{
+        opacity: 0,
+        y: 150,
+      }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+      }}
+      transition={{ delay: isMobile ? 0 : 0.85, duration: 1.5 }}
+      viewport={{ once: true }}
+    >
+      <div className="absolute bottom-[1.875rem] left-[50%] w-[0.375rem] h-[0.375rem] -ml-[0.188rem] bg-white rounded-full animate-scrolldown shadow-[0px_-5px_3px_1px_rgba(42,84,112,0.4)]"></div>
+      <div className="flex flex-col items-center mt-12 pt-[0.375rem]">
+        <div className="w-[0.625rem] h-[0.625rem] border-[0.188rem] border-gray-100 border-t-0 border-l-0 rotate-45 animate-arrow"></div>
+        <div className="w-[0.625rem] h-[0.625rem] border-[0.188rem] border-gray-100 border-t-0 border-l-0 rotate-45 animate-arrow-delay"></div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
