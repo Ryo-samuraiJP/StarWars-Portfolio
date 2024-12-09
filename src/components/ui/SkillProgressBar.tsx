ad019af1faf7a4a152f604ccc1de3dbc2f55596c
Rio <onlytru6340@gmail.com>
2024-12-08 23:55:44 -0800

    Enhance SkillProgressBar component with framer-motion for animated entrance effects

--- src/components/ui/SkillProgressBar.tsx
+++ src/components/ui/SkillProgressBar.tsx
@@ -1,35 +1,51 @@
 import { skillsData } from "../../data/skillsData";
 import { Meteors } from "./meteros";
 import Spotlight, { SpotlightCard } from "./spotlight-card";
+import { motion } from "framer-motion";
 
 const SkillProgressBar = () => {
   return (
     <div className="flex md:px-6">
       <Spotlight className="grid gap-6 lg:grid-cols-1 items-start group">
         {skillsData.map((category) => (
-          <SpotlightCard key={category.id} className="w-full lg:w-[170%]">
-            <div
-              className="relative h-full border border-[rgba(75,30,133,0.5)] rounded-[inherit] 
-              bg-gradient-to-br from-[rgba(75,30,133,0.5)] to-[rgb(0,0,0)] p-2 z-20 overflow-hidden"
+          <SpotlightCard
+            key={category.id}
+            className="w-full lg:w-[170%] bg-transparent"
+          >
+            <motion.div
+              className="relative h-full border-2 border-[rgba(75,30,133,0.5)] rounded-[inherit] 
+              bg-gradient-to-br from-[rgba(75,30,133,0.8)] to-[rgb(0,0,0)] p-2 z-20 overflow-hidden"
+              initial={{
+                opacity: 0,
+                scale: 1.2,
+                y: 100,
+              }}
+              whileInView={{
+                opacity: 1,
+                scale: 1,
+                y: 0,
+              }}
+              transition={{ delay: 0.5, duration: 1.5 }}
+              viewport={{ once: true }}
             >
               {/* white blur */}
-              <div
+              {/* <div
                 className="absolute mb-[19.5rem] -top-44 translate-y-[50%] left-[50%] -translate-x-[50%] pointer-events-none -z-10 w-[50%] aspect-square lg:top-3"
                 aria-hidden="true"
               >
                 <div className="absolute inset-0 translate-z-0 bg-slate-700 rounded-full blur-[2.5rem]"></div>
-              </div>
+              </div> */}
               <div className="flex flex-col h-full items-center text-center">
                 <div className="relative inline-flex p-4">
                   {/* blue blur */}
                   <div
-                    className="w-[40%] h-[40%] border absolute inset-0 m-auto -translate-y-[10%] blur-3xl -z-10 rounded-full bg-purple-600"
+                    className="w-[40%] h-[40%] border absolute inset-0 m-auto blur-3xl -z-10 rounded-full bg-purple-800"
                     aria-hidden="true"
                   ></div>
                   <div className="grow">
-                    <h3 className="text-[1.125rem] font-semibold text-gray-200 mb-4">
+                    <div className="text-[1.125rem] font-semibold text-gray-200 mb-4">
                       <p>{category.category}</p>
-                    </h3>
+                    </div>
                     {/* skill icon grid */}
                     <div className="grid grid-cols-3 gap-4 md:grid-cols-6 justify-center">
                       {category.skills.map((skill, index) => (
@@ -39,10 +55,12 @@ const SkillProgressBar = () => {
                         >
                           <div className="relative flex items-center justify-center">
                             <div className="relative flex items-center justify-center w-16 h-16">
+                              {/* progress bar */}
                               <svg
                                 className="w-[100%] h-[100%] transform rotate-[-90deg] "
                                 viewBox="0 0 200 200"
                               >
+                                {/* background circle stroke */}
                                 <circle
                                   className="stroke-gray-700"
                                   r="90"
@@ -53,6 +71,7 @@ const SkillProgressBar = () => {
                                   strokeDasharray="565.48px"
                                   strokeDashoffset="0"
                                 ></circle>
+                                {/* progress circle stroke with animation */}
                                 <circle
                                   r="90"
                                   cx="100"
@@ -95,7 +114,7 @@ const SkillProgressBar = () => {
                 </div>
               </div>
               <Meteors number={7} />
-            </div>
+            </motion.div>
           </SpotlightCard>
         ))}
       </Spotlight>
