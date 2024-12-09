18956d78f9161bee69601c29765ca19cbef3e559
Rio <onlytru6340@gmail.com>
2024-12-08 23:53:27 -0800

    Enhance IconSphere component with framer-motion for animated entrance effects

--- src/components/ui/IconSphere.tsx
+++ src/components/ui/IconSphere.tsx
@@ -1,4 +1,5 @@
 import { IconCloud } from "./skills-cloud";
+import { motion } from "framer-motion";
 
 const icons = [
   "html5",
@@ -35,9 +36,21 @@ const icons = [
 
 const IconSphere = () => {
   return (
-    <div className="relative md:max-w-md lg:max-w-lg md:px-20 pb-20 pt-8 cursor-pointer">
+    <motion.div
+      className="relative md:max-w-md lg:max-w-lg md:px-20 pb-20 pt-8 cursor-pointer"
+      initial={{
+        opacity: 0,
+        scale: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+        scale: 1,
+      }}
+      transition={{ duration: 1.5 }}
+      viewport={{ once: true }}
+    >
       <IconCloud iconSlugs={icons} />
-    </div>
+    </motion.div>
   );
 };
 
