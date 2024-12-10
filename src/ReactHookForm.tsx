374ca64ba288cd5765ff22bda48f33aecc20f65b
Rio <onlytru6340@gmail.com>
2024-12-09 20:25:11 -0800

    Refactor ReactHookForm component for improved styling and accessibility, removing LinkedIn and email links

--- src/ReactHookForm.tsx
+++ src/ReactHookForm.tsx
@@ -7,7 +7,6 @@ import Spotlight, { SpotlightCard } from "./components/ui/spotlight-card";
 import { ImRocket } from "react-icons/im";
 import validateEmail from "./lib/emailValidator";
 import { MdError, MdCheckCircle } from "react-icons/md";
-import { CiLinkedin, CiMail } from "react-icons/ci";
 import ParticlesAnimation from "./components/ui/particles-animation";
 
 const ReactHookForm = () => {
@@ -78,29 +77,29 @@ const ReactHookForm = () => {
   };
 
   return (
-    <Spotlight className="md:mx-16 lg:mx-80 relative">
+    <Spotlight className="relative md:-mx-5 lg:mx-0">
       <div className="absolute z-10 inset-0 pointer-events-none">
         <ParticlesAnimation />
       </div>
       <SpotlightCard>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="relative flex flex-col gap-y-6 py-8 pb-11 border rounded-3xl
+          className="relative flex flex-col gap-y-6 py-8 pb-11 md:pb-14 lg:pb-20 border rounded-3xl
           border-[rgba(75,30,133,0.5)] bg-[linear-gradient(225deg,rgba(75,30,133,0.8),rgb(0,0,0))]"
         >
-          <div className="flex flex-row gap-x-2 items-center justify-center mt-2 text-base md:text-xl">
+          <div className="flex flex-row gap-x-2 items-center justify-center mt-2 text-base md:text-lg lg:text-xl">
             <ImRocket />
-            <p>Let's Get In Touch</p>
+            <p className="font-thin">SUBMIT CONTACT FORM</p>
           </div>
           <input
             type="text"
             placeholder="Name*"
             {...register("name")}
-            className="rounded-lg md:rounded-xl p-2 md:p-4 mx-7 md:mx-20 bg-transparent border border-slate-500 text-xs md:text-base"
+            className="rounded-lg md:rounded-xl p-2 md:p-2.5 mx-7 md:mx-12 bg-transparent border border-slate-500 text-xs md:text-sm lg:text-base"
           />
           {/* Display error message if name is not provided */}
           {errors.name && (
-            <div className="flex flex-row items-center gap-1 text-left text-xs md:text-sm font-medium px-24 -mx-16 md:mx-0 -my-4 text-red-600">
+            <div className="flex flex-row items-center gap-1 text-left text-xs lg:text-sm font-medium px-24 -mx-16 md:-mx-10 -my-4 text-red-600">
               <MdError />
               {`${errors.name.message}`}
             </div>
@@ -109,11 +108,11 @@ const ReactHookForm = () => {
             type="email"
             placeholder="Email Address*"
             {...register("email")}
-            className="rounded-lg md:rounded-xl p-2 md:p-4 mx-7 md:mx-20 bg-transparent border border-slate-500 text-xs md:text-base"
+            className="rounded-lg md:rounded-xl p-2 md:p-2.5 mx-7 md:mx-12 bg-transparent border border-slate-500 text-xs md:text-sm lg:text-base"
           />
           {/* Display error message if email is not provided */}
           {errors.email && (
-            <div className="flex flex-row items-center gap-1 text-left text-xs md:text-sm font-medium px-24 -mx-16 md:mx-0 -my-4 text-red-600">
+            <div className="flex flex-row items-center gap-1 text-left text-xs lg:text-sm font-medium px-24 -mx-16 md:-mx-10 -my-4 text-red-600">
               <MdError />
               {`${errors.email.message}`}
             </div>
@@ -121,7 +120,7 @@ const ReactHookForm = () => {
           {/* Display email validation status */}
           {emailStatus && (
             <span
-              className={`text-left text-xs md:text-sm font-medium px-24 -mx-16 md:mx-0 -my-4 ${emailStatus.color}`}
+              className={`text-left text-xs lg:text-sm font-medium px-24 -mx-16 md:-mx-10 -my-4 ${emailStatus.color}`}
             >
               <div className="flex flex-row gap-x-1 items-center">
                 {emailStatus.color === "text-red-600" ? (
@@ -139,11 +138,11 @@ const ReactHookForm = () => {
             type="subject"
             placeholder="Subject*"
             {...register("subject")}
-            className="rounded-lg md:rounded-xl p-2 md:p-4 mx-7 md:mx-20 bg-transparent border border-slate-500 text-xs md:text-base"
+            className="rounded-lg md:rounded-xl p-2 md:p-2.5 mx-7 md:mx-12 bg-transparent border border-slate-500 text-xs md:text-sm lg:text-base"
           />
           {/* Display error message if subject is not provided */}
           {errors.subject && (
-            <div className="flex flex-row items-center gap-1 text-left text-xs md:text-sm font-medium px-24 -mx-16 md:mx-0 -my-4 text-red-600">
+            <div className="flex flex-row items-center gap-1 text-left text-xs lg:text-sm font-medium px-24 -mx-16 md:-mx-10 -my-4 text-red-600">
               <MdError />
               {`${errors.subject.message}`}
             </div>
@@ -151,11 +150,11 @@ const ReactHookForm = () => {
           <textarea
             placeholder="Message*"
             {...register("message")}
-            className="rounded-lg md:rounded-xl p-2 md:p-4 pb-20 md:pb-24 mx-7 md:mx-20 resize-none bg-transparent border border-slate-500 text-xs md:text-base"
+            className="rounded-lg md:rounded-xl p-2 md:p-2.5 pb-20 md:pb-24 mx-7 md:mx-12 resize-none bg-transparent border border-slate-500 text-xs md:text-sm lg:text-base"
           />
           {/* Display error message if message is not provided */}
           {errors.message && (
-            <div className="flex flex-row items-center gap-1 text-left text-xs md:text-sm font-medium px-24 -mx-16 md:mx-0 -my-[1rem] text-red-600">
+            <div className="flex flex-row items-center gap-1 text-left text-xs lg:text-sm font-medium px-24 -mx-16 md:-mx-10 -my-[1rem] text-red-600">
               <MdError />
               {`${errors.message.message}`}
             </div>
@@ -163,15 +162,15 @@ const ReactHookForm = () => {
           <button
             disabled={isSubmitting}
             type="submit"
-            className="relative z-10 p-2 md:p-3 mx-7 md:mx-20 rounded-lg md:rounded-xl border border-slate-500 bg-transparent text-slate-400 
-              text-xs md:text-base hover:bg-slate-500 hover:text-white transition-all duration-300 ease-in-out"
+            className="relative z-10 p-2 md:p-2 mx-7 md:mx-12 rounded-lg md:rounded-xl border border-slate-500 bg-transparent text-slate-400 
+              text-xs md:text-sm lg:text-base hover:bg-slate-500 hover:text-white transition-all duration-300 ease-in-out"
           >
             Send Message
           </button>
           {/* Display form submission status */}
           {formStatus && (
             <span
-              className={`text-left text-xs md:text-sm font-medium px-24 -mx-16 md:mx-0 -my-4 ${formStatus.color}`}
+              className={`text-left text-xs lg:text-sm font-medium px-24 -mx-16 md:-mx-10 -my-4 ${formStatus.color}`}
             >
               <div className="flex flex-row gap-x-1 items-center">
                 {formStatus.color === "text-red-600" ? (
@@ -185,34 +184,6 @@ const ReactHookForm = () => {
               </div>
             </span>
           )}
-          <hr className="mx-10 bg-slate-600 border-0 h-px" />
-          <p className="text-slate-500 text-xs md:text-base">
-            Or reach out to me through LinkedIn or email
-          </p>
-          <div className="flex flex-col md:flex-row justify-center gap-y-2 gap-x-8 items-center text-slate-500">
-            <a
-              href="https://linkedin.com/in/ryoichihomma/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex flex-row gap-1 items-center hover:text-white hover:underline transition-all duration-300 ease-in-out"
-            >
-              <CiLinkedin className="text-2xl" />
-              <span className="text-xs md:text-sm">
-                linkedin.com/in/ryoichihomma/
-              </span>
-            </a>
-            <a
-              href="mailto:r.homma.inbox@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex flex-row gap-1 items-center hover:text-white hover:underline transition-all duration-300 ease-in-out"
-            >
-              <CiMail className="text-2xl" />
-              <span className="text-xs md:text-sm">
-                r.homma.inbox@gmail.com
-              </span>
-            </a>
-          </div>
         </form>
       </SpotlightCard>
     </Spotlight>
