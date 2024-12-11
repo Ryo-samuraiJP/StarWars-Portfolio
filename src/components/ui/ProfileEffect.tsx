fd8eab12a918624f6f64f0d47530ecb7e211adc4
Rio <onlytru6340@gmail.com>
2024-12-11 05:55:04 -0800

    Enhance ProfileEffect component with responsive sizing and scale animation for improved visual presentation

--- src/components/ui/ProfileEffect.tsx
+++ src/components/ui/ProfileEffect.tsx
@@ -7,18 +7,18 @@ const ProfileEffect = () => {
 
   return (
     <motion.div
-      className="bg-cover bg-center bg-no-repeat shadow-[inset_0_0_0_9px_rgba(225,225,225,0.3)] 
-        w-[12.5rem] h-[18.75rem] md:w-[15.625rem] md:h-[21.875rem] lg:w-[18.75rem] lg:h-[27.5rem] animate-profile-effect"
+      className="bg-cover bg-center bg-no-repeat shadow-[inset_0_0_0_9px_rgba(225,225,225,0.3)]
+        w-[12.5rem] h-[18.75rem] sm:w-[14rem] sm:h-[20rem] md:w-[15.625rem] md:h-[21.875rem] lg:w-[18.75rem] lg:h-[27.5rem] animate-profile-effect"
       style={{ backgroundImage: `url(${profile})` }}
       initial={{
         opacity: 0,
+        scale: 0,
         y: isMobile ? -150 : 0,
-        x: isMobile ? 0 : 200,
       }}
       whileInView={{
         opacity: 1,
+        scale: 1,
         y: 0,
-        x: 0,
       }}
       transition={{ delay: isMobile ? 0 : 0.25, duration: 1.5 }}
       viewport={{ once: true }}
