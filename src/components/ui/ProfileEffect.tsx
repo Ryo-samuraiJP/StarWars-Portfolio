d91f5b179a3758072203449d249ba4f6625f00be
Rio <onlytru6340@gmail.com>
2024-12-11 09:49:19 -0800

    Adjust responsive sizing for ProfileEffect component for improved layout on smaller screens

--- src/components/ui/ProfileEffect.tsx
+++ src/components/ui/ProfileEffect.tsx
@@ -8,7 +8,7 @@ const ProfileEffect = () => {
   return (
     <motion.div
       className="bg-cover bg-center bg-no-repeat shadow-[inset_0_0_0_9px_rgba(225,225,225,0.3)]
-        w-[12.5rem] h-[18.75rem] sm:w-[14rem] sm:h-[20rem] md:w-[15.625rem] md:h-[21.875rem] lg:w-[18.75rem] lg:h-[27.5rem] animate-profile-effect"
+        w-[12.5rem] h-[18.75rem] sm:w-[12rem] sm:h-[18rem] md:w-[15.625rem] md:h-[21.875rem] lg:w-[18.75rem] lg:h-[27.5rem] animate-profile-effect"
       style={{ backgroundImage: `url(${profile})` }}
       initial={{
         opacity: 0,
