227278d34424e5213e65474b70e6d6279449ce41
Rio <onlytru6340@gmail.com>
2024-12-11 10:20:02 -0800

    Enhance TechSkillSection component with responsive design adjustments for improved layout on smaller screens

--- src/sections/TechSkillSection.tsx
+++ src/sections/TechSkillSection.tsx
@@ -27,30 +27,28 @@ const TechSkillSection = () => {
         transition={{ duration: 1.5 }}
         viewport={{ once: true }}
       >
-        <p className="md:text-xl my-2 font-bold font-serif text-yellow-500">
+        <p className="sm:text-xl my-2 font-bold font-serif text-yellow-500">
           Episode II
         </p>
-        <p className="text-yellow-500 star-wars-font text-[1.75rem] md:text-[3rem] lg:text-[3.25rem]">
-          technical skills
+        <p className="text-yellow-500 star-wars-font text-[1.75rem] sm:text-4xl md:text-5xl lg:text-[3.25rem]">
+          technical skill
         </p>
-        <p className="mt-4 mb-6 md:mb-10 text-yellow-500 text-sm md:text-base">
+        <p className="mt-4 mb-6 sm:mb-10 text-yellow-500 text-sm sm:text-base">
           Skills cloud and primary technologies
         </p>
       </motion.div>
-      <div className="w-full max-w-[72rem] mx-auto px-4 md:px-6">
-        <div className="flex flex-col lg:flex-row items-start lg:items-center">
-          <div className="relative inline-flex md:ml-24 lg:-ml-10 -mt-10 lg:mt-0">
-            <div
-              className="w-[55%] h-[55%] lg:w-[50%] lg:h-[50%] border absolute inset-0 m-auto blur-3xl -z-10 rounded-full bg-violet-800"
-              aria-hidden="true"
-            ></div>
-            <div className="grow">
-              <IconSphere />
-            </div>
+      <div className="flex flex-col lg:flex-row items-center lg:gap-x-8 px-4 md:px-6">
+        <div className="relative inline-flex lg:-mx-14 xl:-mx-5 -mt-10 lg:mt-0 sm:max-w-[20rem] md:max-w-[30rem] lg:max-w-max">
+          <div
+            className="w-[55%] h-[55%] lg:w-[50%] lg:h-[50%] absolute inset-0 m-auto blur-3xl -z-10 rounded-full bg-violet-800"
+            aria-hidden="true"
+          ></div>
+          <div className="grow">
+            <IconSphere />
           </div>
-          <div className="lg:w-1/3">
-            <SkillsProgress />
-          </div>
+        </div>
+        <div className="lg:w-1/3">
+          <SkillsProgress />
         </div>
       </div>
     </section>
