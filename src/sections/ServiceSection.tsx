a60494e6cfe28f15c1746bfc531c5eb2703c9de9
Rio <onlytru6340@gmail.com>
2024-12-11 16:36:24 -0800

    Enhance ServiceSection component with responsive text size adjustments for improved display on smaller screens

--- src/sections/ServiceSection.tsx
+++ src/sections/ServiceSection.tsx
@@ -26,13 +26,13 @@ const ServiceSection = () => {
         transition={{ duration: 1.5 }}
         viewport={{ once: true }}
       >
-        <p className="md:text-xl my-2 font-bold font-serif text-yellow-500">
+        <p className="sm:text-xl my-2 font-bold font-serif text-yellow-500">
           Episode III
         </p>
-        <p className="text-yellow-500 star-wars-font text-[1.75rem] md:text-[3rem] lg:text-[3.25rem]">
-          services
+        <p className="text-yellow-500 star-wars-font text-[1.75rem] sm:text-4xl md:text-5xl lg:text-[3.25rem]">
+          service
         </p>
-        <p className="mt-4 mb-6 md:mb-10 text-yellow-500 text-sm md:text-base">
+        <p className="mt-4 mb-6 sm:mb-10 text-yellow-500 text-sm sm:text-base">
           What I offer and how I contribute to your projects
         </p>
       </motion.div>
