197e8b2b849a9227cfb8e4556732b7534a0f0eeb
Rio <onlytru6340@gmail.com>
2024-12-11 16:59:33 -0800

    Enhance Footer component with responsive design adjustments for improved display on smaller screens

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -26,13 +26,13 @@ const Footer = () => {
         transition={{ duration: 1.5 }}
         viewport={{ once: true }}
       >
-        <p className="md:text-xl my-2 font-bold font-serif text-yellow-500">
+        <p className="sm:text-xl my-2 font-bold font-serif text-yellow-500">
           Episode VII
         </p>
-        <h1 className="text-yellow-500 star-wars-font text-[1.75rem] md:text-[3rem] lg:text-[3.25rem]">
+        <h1 className="text-yellow-500 star-wars-font text-[1.75rem] sm:text-4xl md:text-5xl lg:text-[3.25rem]">
           the code awakens
         </h1>
-        <p className="mt-4 md:mx-16 lg:mx-80 text-yellow-500 text-xs md:text-base">
+        <p className="mt-4 sm:mx-16 lg:mx-[12rem] xl:mx-80 text-yellow-500 text-xs sm:text-base">
           Reach out to me today and let's discuss how I can help you achieve
           your goals. Let's build something impactful together!
         </p>
@@ -48,11 +48,12 @@ const Footer = () => {
           y: 0,
         }}
         transition={{ delay: isMobile ? 0.25 : 0.4, duration: 1.5 }}
+        viewport={{ once: true }}
       >
         <SocialIcons />
       </motion.div>
       <motion.div
-        className="flex flex-col md:flex-row justify-center text-xs md:text-sm lg:text-base font-light gap-x-1"
+        className="flex flex-col sm:flex-row justify-center text-xs sm:text-sm lg:text-base font-light gap-x-1"
         initial={{
           opacity: 0,
           y: -100,
@@ -62,6 +63,7 @@ const Footer = () => {
           y: 0,
         }}
         transition={{ delay: isMobile ? 0.5 : 0.65, duration: 1.5 }}
+        viewport={{ once: true }}
       >
         <p>&#169; 2024 Ryoichi (Rio) Homma.</p>
         <div className="flex flex-row justify-center">
