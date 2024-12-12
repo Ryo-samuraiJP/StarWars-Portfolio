a5e855f9388c8057a2d711863cb9b01678ae63b3
Rio <onlytru6340@gmail.com>
2024-12-11 16:48:16 -0800

    Enhance CredentialSection component with responsive design adjustments and improved animations for better display on different screen sizes

--- src/sections/CredentialSection.tsx
+++ src/sections/CredentialSection.tsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 
 const CredentialSection = () => {
   const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
+  const isLaptop = useMediaQuery({ query: "(min-width: 1024px)" });
 
   return (
     <section id="credential">
@@ -26,24 +27,26 @@ const CredentialSection = () => {
         transition={{ duration: 1.5 }}
         viewport={{ once: true }}
       >
-        <p className="md:text-lg my-2 font-bold font-serif text-yellow-500">
+        <p className="sm:text-lg my-2 font-bold font-serif text-yellow-500">
           Episode IV
         </p>
-        <p className="text-yellow-500 star-wars-font text-[1.75rem] md:text-[3rem] lg:text-[3.25rem]">
-          credentials
+        <p className="text-yellow-500 star-wars-font text-[1.75rem] sm:text-4xl md:text-5xl lg:text-[3.25rem]">
+          credential
         </p>
-        <p className="mt-4 mb-6 md:mb-10 text-yellow-500 text-sm md:text-base">
+        <p className="mt-4 mb-6 md:mb-10 text-yellow-500 text-sm sm:text-base">
           Professional & educational backgrounds and certifications
         </p>
       </motion.div>
       <motion.div
         initial={{
           opacity: 0,
-          y: -100,
+          y: isLaptop ? 100 : 0,
+          x: isLaptop ? 0 : isMobile ? 100 : 200,
         }}
         whileInView={{
           opacity: 1,
           y: 0,
+          x: 0,
         }}
         transition={{ delay: isMobile ? 0.25 : 0.4, duration: 1.5 }}
         viewport={{ once: true }}
