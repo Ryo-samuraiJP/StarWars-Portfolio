9a8ca0b7bbba2c748a747a114e7b4a4d47cd6a74
Rio <onlytru6340@gmail.com>
2024-12-13 01:52:25 -0800

    Refactor NumbersCounter component for improved gap spacing and layout consistency

--- src/components/ui/NumbersCounter.tsx
+++ src/components/ui/NumbersCounter.tsx
@@ -26,15 +26,15 @@ const NumbersCounter = () => {
 
   return (
     <div className="grid sm:grid-cols-2 xl:grid-cols-4 gap-y-6 md:gap-x-16 lg:gap-x-52 xl:gap-0">
-      <div className="flex items-center justify-end sm:justify-normal gap-x-4 sm:gap-x-11 md:gap-x-3 sm:mb-7 xl:mb-0">
+      <div className="flex items-center justify-end sm:justify-normal gap-x-4 sm:gap-x-11 md:gap-x-3 xl:gap-x-0 sm:mb-7 xl:mb-0">
         <div className="font-bold tracking-tighter w-4 md:w-[3.5rem] text-left text-5xl sm:text-[3.2rem]">
           <NumbersCounterAnimation value={4} />+
         </div>
-        <p className="w-[6.8rem] sm:w-[9rem] font-light text-sm md:text-base text-right sm:text-center">
+        <p className="w-[6.8rem] sm:w-[9rem] lg:w-40 font-light text-sm md:text-base text-right sm:text-center">
           YEARS OF BUILDING SOFTWARE
         </p>
       </div>
-      <div className="flex items-center justify-end sm:justify-normal gap-x-12 sm:gap-x-12 md:gap-x-11 lg:gap-x-[3.25rem] sm:mb-7 xl:mb-0 sm:ml-24 xl:ml-0">
+      <div className="flex items-center justify-end sm:justify-normal gap-x-12 sm:gap-x-12 md:gap-x-11 lg:gap-x-14 xl:gap-x-11 sm:mb-7 xl:mb-0 sm:ml-24 xl:ml-0">
         <div className="font-bold tracking-tighter w-6 text-left text-5xl sm:text-[3.2rem]">
           <NumbersCounterAnimation value={15} />
         </div>
@@ -42,7 +42,7 @@ const NumbersCounter = () => {
           CERTIFICATIONS
         </p>
       </div>
-      <div className="flex items-center justify-end sm:justify-normal gap-x-6 sm:gap-x-8 md:gap-x-3.5 lg:gap-x-5">
+      <div className="flex items-center justify-end sm:justify-normal gap-x-6 sm:gap-x-8 md:gap-x-3.5 lg:gap-x-5 xl:gap-x-0">
         <div className="font-bold tracking-tighter w-8 md:w-[3.5rem] text-left text-5xl sm:text-[3.2rem]">
           <NumbersCounterAnimation value={postCount ?? 0} />
         </div>
@@ -50,7 +50,7 @@ const NumbersCounter = () => {
           POSTS ON DEV COMMUNITY
         </p>
       </div>
-      <div className="flex items-center justify-end sm:justify-normal gap-x-12 sm:gap-x-14 md:gap-x-8 lg:gap-x-[3.25rem]">
+      <div className="flex items-center justify-end sm:justify-normal gap-x-12 sm:gap-x-14 md:gap-x-8 lg:gap-x-14 xl:gap-x-8">
         <div className="font-bold tracking-tighter w-[6.25rem] md:w-32 text-left text-5xl sm:text-[3.2rem]">
           <NumbersCounterAnimation value={4400} />+
         </div>
