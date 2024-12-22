99cd25063b03c47d4375650ebb991dad433ff3e0
Rio <onlytru6340@gmail.com>
2024-12-21 23:32:36 -0800

    Update email sending logic to use Axios for EmailJS REST API integration; and uninstall @emailjs/brwoser dependencies

--- src/lib/email-js.ts
+++ src/lib/email-js.ts
@@ -1,11 +1,11 @@
-import emailjs from "@emailjs/browser";
+import axios from "axios";
 import { TContactFormSchema } from "./types-of-contact-form-schema";
 import { validateEmail } from "../api/emailValidatorWithZeroBounceApi";
 
 // Load EmailJS service ID, template ID, and public key from environment variables (set in .env file)
-const SERVICE_ID = process.env.EMAILJS_SERVICE_ID || "";
-const TEMPLATE_ID = process.env.EMAILJS_TEMPLATE_ID || "";
-const PUBLIC_KEY = process.env.EMAILJS_PUBLIC_KEY || "";
+const SERVICE_ID = process.env.EMAILJS_SERVICE_ID;
+const TEMPLATE_ID = process.env.EMAILJS_TEMPLATE_ID;
+const PUBLIC_KEY = process.env.EMAILJS_PUBLIC_KEY;
 
 export const sendEmailWithEmailjs = async (data: TContactFormSchema) => {
   // Validate email address using emailValidator function before sending the email with EmailJS
@@ -14,6 +14,20 @@ export const sendEmailWithEmailjs = async (data: TContactFormSchema) => {
     throw new Error("Email address is invalid");
   }
 
-  // Send the email using EmailJS
-  return emailjs.send(SERVICE_ID, TEMPLATE_ID, data, PUBLIC_KEY);
+  // Send email using EmailJS REST API
+  try {
+    const response = await axios.post(
+      "https://api.emailjs.com/api/v1.0/email/send",
+      {
+        service_id: SERVICE_ID,
+        template_id: TEMPLATE_ID,
+        user_id: PUBLIC_KEY,
+        template_params: data,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Failed to send email:", error);
+    throw error;
+  }
 };
